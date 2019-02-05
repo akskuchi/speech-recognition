d9cbd0db59e6f00787fb914849a1fdfd5af9df49
Aditya Kaushik <aditya.surikuchi@aalto.fi>
2019-02-05 19:35:26 +0200

    fixing bidir

--- sources/model.py
+++ sources/model.py
@@ -82,13 +82,14 @@ class RNNModel(nn.Module):
         decoded = self.decoder(output.view(output.size(0) * output.size(1), output.size(2)))
         return decoded.view(output.size(0), output.size(1), decoded.size(1)), hidden
 
-    def init_hidden(self, bsz):
+    def init_hidden(self, bsz, bi_dir=False):
         weight = next(self.parameters())
+        num_directions = 2 if bi_dir else 1
         if self.rnn_type == 'LSTM':
-            return (weight.new_zeros(self.nlayers, bsz, self.nhid),
-                    weight.new_zeros(self.nlayers, bsz, self.nhid))
+            return (weight.new_zeros(self.nlayers * num_directions, bsz, self.nhid),
+                    weight.new_zeros(self.nlayers * num_directions, bsz, self.nhid))
         else:
-            return weight.new_zeros(self.nlayers, bsz, self.nhid)
+            return weight.new_zeros(self.nlayers * num_directions, bsz, self.nhid)
 
 
 class RNNModelOriginal(nn.Module):
